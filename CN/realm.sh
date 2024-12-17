cbce1b08020b73ce3cc97bfd995ca2e8c00edb28
qqrrooty <64602472+qqrrooty@users.noreply.github.com>
2024-12-18 01:03:13 +0800

    Update realm.sh

--- CN/realm.sh
+++ CN/realm.sh
@@ -60,7 +60,7 @@ show_menu() {
 deploy_realm() {
     mkdir -p /root/realm
     cd /root/realm
-    wget -O realm.tar.gz https://gh-proxy.com/github.com/zhboner/realm/releases/download/v2.7.0/realm-x86_64-unknown-linux-gnu.tar.gz
+    wget -O realm.tar.gz https://cf.ghproxy.cc/https://github.com/zhboner/realm/releases/download/v2.7.0/realm-x86_64-unknown-linux-gnu.tar.gz
     tar -xvf realm.tar.gz
     chmod +x realm
     # 创建服务文件
