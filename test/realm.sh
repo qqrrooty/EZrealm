22af28581f06fd35be770600fa4a534813b839f8
qqrrooty <64602472+qqrrooty@users.noreply.github.com>
2025-01-29 03:11:07 +0800

    Update realm.sh

--- test/realm.sh
+++ test/realm.sh
@@ -3,7 +3,7 @@
 # ========================================
 # 全局配置
 # ========================================
-CURRENT_VERSION="0.1.0"
+CURRENT_VERSION="0.1.1"
 UPDATE_URL="https://raw.githubusercontent.com/qqrrooty/EZrealm/main/test/realm.sh"
 VERSION_CHECK_URL="https://raw.githubusercontent.com/qqrrooty/EZrealm/main/version.txt"
 REALM_DIR="/root/realm"
